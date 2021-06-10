ef670a83130b67a131f8c55b182592741e049aaf
andika-eka <putuandika387@gmail.com>
2021-06-10 20:31:21 +0800

    add  excel import-export to sidebar

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -69,6 +69,20 @@
                                 <a class="nav-link" href="/Lilliefors">Lilliefors</a>
                             </nav>
                         </div>
+
+                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts2"
+                            aria-expanded="false" aria-controls="collapseLayouts">
+                            <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                            Excel
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                        </a>
+                        <div class="collapse" id="collapseLayouts2" aria-labelledby="headingOne"
+                            data-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="/export">export</a>
+                                <a class="nav-link" href="/import">import</a>
+                            </nav>
+                        </div>
                         <a class="nav-link" href="/about">
                             <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
                             About
